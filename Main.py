6751c232be3f5f1417cd0c37293ff2bf55d9b72d
SirLlamaV <mayeuxkaleb@gmail.com>
2023-06-09 14:04:37 -0600

    Most of code is done, input output selection finished

--- Main.py
+++ Main.py
@@ -1,1 +1,21 @@
-print("Hello World!")
+import os
+import shutil
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+
+
+try:
+    inputDirectory = askdirectory(title='Select Your Input Directory')+"/"
+    outputDirectory = askdirectory(title='Select Your Input Directory')+"/."
+        
+except:
+    print("Error")
+    
+inputDirectoryFiles = os.listdir(inputDirectory)
+for file in inputDirectoryFiles:
+    if file.endswith('.zip') or file.endswith('.7z') or file.endswith('.rar'):
+        shutil.move(os.path.join(inputDirectory,file), os.path.join(outputDirectory,file))
+        
+outputDirectoryFiles = os.listdir(outputDirectory)
+for file in outputDirectoryFiles:
+    
